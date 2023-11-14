ee601d1619fed2e979b56e809e63ea5adfa77fa1
Lucpc <lucpcaldas@gmail.com>
2023-11-14 15:59:29 -0300

    classe de rerva criada

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,53 @@
+package model.entities;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private int roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(int roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration (){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void upadateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "roomNumber=" + roomNumber +
+                ", checkIn=" + sdf.format(checkIn) +
+                ", checkOut=" + checkOut +
+                '}';
+    }
+}
